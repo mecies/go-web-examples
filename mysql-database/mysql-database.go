db9056041af739b4aad70d046e914c5a7c0f9648
mecies <maciekhnat@gmail.com>
2024-02-18 19:51:41 +0100

    MySQL database CRUD

---
+++ mysql-database/mysql-database.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	viper "github.com/spf13/viper"
+)
+
+func viperEnvVariable(key string) string {
+
+	// SetConfigFile explicitly defines the path, name and extension of the config file.
+	// Viper will use this and not check any of the config paths.
+	// .env - It will search for the .env file in the current directory
+	viper.SetConfigFile("../.env")
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
+}
+
+func main() {
+
+	dbUser, dbPassword, dbName := viperEnvVariable("DB_USER"), viperEnvVariable("DB_PASSWORD"), viperEnvVariable("DB_NAME")
+
+	dbUrl := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?parseTime=true", dbUser, dbPassword, dbName)
+
+	// Configure the database connection
+	db, err := sql.Open("mysql", dbUrl)
+
+	if err != nil {
+		fmt.Println("db connection fail", err)
+	}
+
+	// Initialize the first connection to the database, to see if everything works correctly.
+	err = db.Ping()
+
+	if err != nil {
+		fmt.Println("ping fail", err)
+	}
+
+	{ // Create a new table
+		query := `
+	          CREATE TABLE users (
+	              id INT AUTO_INCREMENT,
+	              username TEXT NOT NULL,
+	              password TEXT NOT NULL,
+	              created_at DATETIME,
+	              PRIMARY KEY (id)
+	          );`
+
+		if _, err := db.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	{ // Insert a new user
+		username := "johndoe"
+		password := "secret"
+		createdAt := time.Now()
+
+		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := result.LastInsertId()
+		fmt.Println(id)
+	}
+
+	{ // Query a single user
+		var (
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		)
+
+		query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(id, username, password, createdAt)
+	}
+
+	{ // Query all users
+		type user struct {
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		}
+
+		rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+		for rows.Next() {
+			var u user
+
+			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v", users)
+	}
+
+	{
+		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
