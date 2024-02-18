4cefedfe7c3cd298b755e1e0fa35e656ca393bee
mecies <maciekhnat@gmail.com>
2024-02-18 18:08:20 +0100

    Routing gorillax/mux

---
+++ routing/routing.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	bookrouter := r.PathPrefix("/books").Methods("GET").Host("localhost").Schemes("http").Subrouter()
+
+	bookrouter.HandleFunc("/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
